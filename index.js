578b1d60eda21e1a1072c4b1854e0e0e3e537f43
TarNikolett <you@example.com>
2021-06-21 19:54:09 +0200

    check hours and years - works

--- index.js
+++ index.js
@@ -1,52 +1,108 @@
-function setResolveDate (date, month, dayNumber){
+function setResolveDate (date, month, dayNumber, hours, minutes, year){
   date.setMonth(month);
   date.setDate(dayNumber);
-}
-
-function CalculateDueDate (currentDate, turnaroundTime){
-  const reportedDate = new Date(currentDate);
-  let dayNumber = reportedDate.getDate();
-  let month = reportedDate.getMonth();
-  let day = reportedDate.getDay();
-  let hours = reportedDate.getHours();
-  let resolvedDay = new Date(`${month} ${dayNumber}, 2021 ${hours}:00:00`);
-
-  if(day === 0){
-    console.log('Today is Sunday - non-working day!');
-    return
-  }else if(day === 6){
-    console.log('Today is Saturday - non-working day!');
-    return
-  }else if(hours < 9 || hours > 17){
-    console.log('A problem can only be reported during working hours - between 9AM to 5PM');
-    return
-  };
-
-  for(let i = 1; i <= turnaroundTime; i++){
-    let lastDayOfMonth = getLastDayOfMonth(new Date(2021, month + 1, dayNumber));
-    if(i % 8 === 0){
-      if(day === 5){
-        dayNumber = dayNumber + 3;
-        day = 1;
-        setResolveDate(resolvedDay, month, dayNumber);
-      }else{
-        dayNumber++;
-        day++;
-        setResolveDate(resolvedDay, month, dayNumber);
-      }
-    }
-    else if(dayNumber === lastDayOfMonth){
-      month++;
-      dayNumber = 0;
-      setResolveDate(resolvedDay, month, dayNumber);
-    }
- };
-  console.log(`Reported day: ${reportedDate}`);
-  console.log(`Resolved day: ${resolvedDay}`);
+  date.setHours(hours);
+  date.setMinutes(minutes);
+  date.setYear(year);
 };
 
 function getLastDayOfMonth(date) {
   return new Date(2021, date.getMonth(), 0).getDate();
 };
 
-CalculateDueDate("June 28, 2021 10:00:00", 9);
+function checkWeekends(index){
+  if(index === 0){
+    return true
+  }else if(index === 6){
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+function CalculateDueDate (currentDate, turnaroundTime){
+  const reportedDate = new Date(currentDate);
+
+  let year = reportedDate.getFullYear();
+  let month = reportedDate.getMonth();
+  let dayNumber = reportedDate.getDate();
+  let dayNameIndex = reportedDate.getDay();
+  let hours = reportedDate.getHours();
+  let minutes = reportedDate.getMinutes();
+  let startHour = 9;
+  let endHour = 17;
+  let resolvedDay = new Date(`${month} ${dayNumber}, ${year} ${hours}:${minutes}`);
+
+  if (checkWeekends(dayNameIndex)){
+    console.log("Today is a non-working day!");
+    return
+  };
+
+  if(hours < startHour || hours >= endHour){
+    console.log(`A problem can only be reported during working hours - between ${startHour}AM to ${endHour}PM`);
+    return
+  };
+
+  for(let i = 1; i <= turnaroundTime; i++){
+    let lastDayOfMonth = getLastDayOfMonth(new Date(year, month + 1, dayNumber));
+    if(i % 8 === 0){
+      if (dayNameIndex !== 5 && hours === endHour){
+        hours = startHour;
+        hours++;
+        dayNumber++;
+        dayNameIndex++;
+        setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+      }
+      else if (dayNameIndex !== 5 && hours !== endHour){
+        hours++;
+        dayNameIndex++;
+        setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+      }
+      else if(dayNameIndex === 5 && hours === endHour && lastDayOfMonth !== dayNumber && month !== 11){
+        dayNumber = dayNumber + 3;
+        dayNameIndex = 1;
+        hours = startHour;
+        hours++;
+        setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+      }
+      else if(dayNameIndex === 5 && hours === endHour && lastDayOfMonth === dayNumber && month === 11){
+        dayNumber = dayNumber + 3;
+        dayNameIndex = 1;
+        hours = startHour;
+        hours++;
+        year++;
+        setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+      }
+    }
+    else if(dayNumber === lastDayOfMonth && month === 11 && hours === endHour){
+      month = 0;
+      dayNumber = 1;
+      year++;
+      setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+    }
+    else if(dayNameIndex === 5 && hours !== endHour){
+      hours++;
+      setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+    }
+    else if(i % 8 !== 0 && hours < endHour){
+      hours++;
+      setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+    }
+    else if(dayNumber === lastDayOfMonth && hours === endHour){
+      month++;
+      dayNumber = 1;
+      setResolveDate(resolvedDay,month,dayNumber, hours, minutes, year);
+    }
+    else if(i % 8 !== 0 && hours === endHour){
+      hours = startHour;
+      hours++;
+      dayNumber++;
+      setResolveDate(resolvedDay, month, dayNumber, hours, minutes, year);
+    }
+ };
+  console.log(`Reported day: ${reportedDate}`);
+  console.log(`Resolved day: ${resolvedDay}`);
+};
+
+CalculateDueDate("Jun 21, 2021 17:35", 8);
